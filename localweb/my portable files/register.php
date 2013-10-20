5884037f033260911ee88920763855372b816c67
unknown <dmsalsman@anc-sci204-13.ua.ad.alaska.edu>
2013-10-19 22:11:12 -0800

    Added input verfication to registration

--- localweb/my portable files/register.php
+++ localweb/my portable files/register.php
@@ -4,6 +4,39 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />  
 <title>User Management System (Tom Cameron for NetTuts)</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
+<script type="text/javascript">
+var results = true;
+
+function checkForm(f)
+{
+    checkEmailAndPassword();
+    if(results){
+        f.submit();
+    }else{
+        results = true;
+    }
+    return false;
+}
+
+function checkEmailAndPassword() {
+    var email = document.getElementById('email');
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!filter.test(email.value)) {
+        alert('Please provide a valid email address');
+        email.focus();
+        results = false;
+    }
+
+    var password = document.getElementById('password');
+    filter = /^.{6,}$/;
+    
+    if (!filter.test(password.value)) {
+        alert('Please provide a password at least 6 characters in length.');
+        password.focus();
+        results = false;
+    }
+}
+</script>  
 </head>  
 <body>  
 <div id="main">
@@ -47,7 +80,7 @@ else
     
    <p>Please enter your details below to register.</p>
     
-	<form method="post" action="register.php" name="registerform" id="registerform">
+	<form method="post" action="register.php" name="registerform" id="registerform" onsubmit="return checkForm(this); return false;">
 	<fieldset>
 		<label for="email">Email Address:</label><input type="text" name="email" id="email" /><br />
         <label for="password">Password:</label><input type="password" name="password" id="password" /><br />
