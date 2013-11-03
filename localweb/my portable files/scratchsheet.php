96711ac00649f147eacac771400ebcca0ef03984
unknown <dmsalsman@anc-sci204-14.ua.ad.alaska.edu>
2013-11-02 18:27:27 -0800

    Groundwork laid for user editing as admin

--- localweb/my portable files/scratchsheet.php
+++ localweb/my portable files/scratchsheet.php
@@ -7,42 +7,33 @@
 <body>
 <div id="main">
 <?php
-  $getproposals = $db->prepare("SELECT * FROM proposal");
-       echo"<h2> Proposals </h2>";
-      $getproposals->execute();
-      while($results = $getproposals->fetch(PDO::FETCH_ASSOC)){
-        $getrespectivefile = $db->prepare("SELECT * FROM files WHERE ID = :fid");
-        $getrespectivefile->execute(array(":fid"=>$results["FileID"]));
-        $rowInFileTable = $getrespectivefile->fetch(PDO::FETCH_ASSOC);
-        $getstudentwhosubmitted = $db->prepare("SELECT * FROM users WHERE UserID = :uid");
-        $getstudentwhosubmitted->execute(array(":uid"=>$rowInFileTable["UserID"]));
-        $rowInStudentTable = $getstudentwhosubmitted->fetch(PDO::FETCH_ASSOC);
-        $getreviewers = $db->prepare("SELECT * FROM review WHERE FileProposalID = :fid AND StudentID = :uid");
-        $getreviewers->execute(array(":fid"=>$results["FileID"], ":uid"=>$rowInFileTable["UserID"]));
-          echo "<ul><li>";
-            echo "Proposal:\t"."<a href=download.php?id=".$results["FileID"].">".$rowInFileTable["Name"]."</a><br>";
-            echo "Submitted By:\t".$rowInStudentTable["Username"]."<br>";
-            echo "Reviewers: <ul>";
-            while($rowInReviewTable = $getreviewers->fetch(PDO::FETCH_ASSOC)) {
-              $getReviewerAsUser = $db->prepare("SELECT * FROM users WHERE UserID = :uid");
-              $getReviewerAsUser->execute(array(":uid"=>$rowInReviewTable["ReviewerID"]));
-              $reviewerAsUser = $getReviewerAsUser->fetch(PDO::FETCH_ASSOC);
-              echo "<li>".$reviewerAsUser["Username"]."</br></li>";
-            }
-            echo "</ul>";
-           echo "</li></ul>";}
-      echo"<h2> Proposals </h2>";
-     /* $getproposals = $db->prepare("SELECT * FROM review WHERE ReviewerID > 0 AND FileReviewID > 0");
-      $getproposals->execute();
-            while($results = $getproposals->fetch(PDO::FETCH_ASSOC)){
+  $getusers = $db->prepare("SELECT * FROM users");
+       echo"<h2> Users </h2>";
+      $getusers->execute();
+      $permissionvalues = array(0 => "Student", 1 => "Faculty", 2 => "Admin");
+      while($results = $getusers->fetch(PDO::FETCH_ASSOC)){
+        echo "<ul><li>";
+          echo "Name:           ".$results["Username"]."</br>";
+          echo "E-mail:         ".$results["EmailAddress"]."</br>";
+          echo "Permissions:    ";
+          $permission = $results["Permissions"];
+          echo $permissionvalues[$permission];
+          echo "</br>";
+          echo "</ul>";
+         echo "</li></ul>";
+      }
+      
+     /* $getusers = $db->prepare("SELECT * FROM review WHERE ReviewerID > 0 AND FileReviewID > 0");
+      $getusers->execute();
+            while($results = $getusers->fetch(PDO::FETCH_ASSOC)){
               echo "<ul><li>";
               echo "File:\t"."<a href=download.php?id=".$results["FileProposalID"].">Download</a>"."<br>";
       echo "</li></ul>";
       }
       echo"<h2> Here are the assigned files awaiting review.</h2>";
-      $getproposals = $db->prepare("SELECT * FROM review WHERE ReviewerID > 0 AND FileReviewID = 0");
-      $getproposals->execute();
-            while($results = $getproposals->fetch(PDO::FETCH_ASSOC)){
+      $getusers = $db->prepare("SELECT * FROM review WHERE ReviewerID > 0 AND FileReviewID = 0");
+      $getusers->execute();
+            while($results = $getusers->fetch(PDO::FETCH_ASSOC)){
               echo "<ul><li>";
               echo "File:\t"."<a href=download.php?id=".$results["FileProposalID"].">Download</a>"."<br>";
       echo "</li></ul>";
